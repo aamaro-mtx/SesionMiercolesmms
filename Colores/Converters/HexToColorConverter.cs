d2c27c475acced1a39f4410b1da885db7f061218
Abel Amaro Julian <amarotux@gmail.com>
2014-03-13 23:51:56 -0600

    Ahora ya muestra el nombredel color.

---
+++ Colores/Converters/HexToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Data;
+
+namespace Colores.Converters
+{
+    public class HexToColorConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var tmp = value as string;
+            return !string.IsNullOrEmpty(tmp) ? ColorTranslator.FromHtml(tmp).Name : string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var tmp = value as string;
+            return !string.IsNullOrEmpty(tmp) ? ColorTranslator.FromHtml(tmp).Name : string.Empty;
+        }
+
+        #endregion
+    }
+}
